a0a6b5bc3a0e309c3e1fdd6423624eca54fb7b3f
xxx <91067875+Marshal1l@users.noreply.github.com>
2025-09-15 09:11:08 +0800

    Update proxy.c

--- proxy.c
+++ proxy.c
@@ -7,7 +7,7 @@
 #define MAX_CACHE (MAX_CACHE_SIZE / MAX_OBJECT_SIZE)  /* 大约 10 */
 #define NTHREADS  8
 #define SBUFSIZE  16
-
+//
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
